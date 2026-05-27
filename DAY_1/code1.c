a4f7847b2dac15562d82b69b04aa448740e66363
13HappyKamboj <13happykamboj@gmail.com>
2026-05-27 15:44:59 +0530

    Added DAY_1 code

---
+++ DAY_1/code1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int num;
+
+    //Asking the user to enter Nth number
+    printf("Enter the Nth number : ");
+    scanf("%d", &num);
+
+    int sum = 0;
+
+    //Calculating Sum
+    for(int i = 1; i <= num; i++){
+        sum = sum + i;
+    }
+
+    //Print Sum
+    printf("Sum of the first %d natural numbers : %d", num, sum);
+
+    return 0;
+}
