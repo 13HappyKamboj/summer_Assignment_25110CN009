02c68bcbd6f4dd0e0f3e67f9988943104df4d7f0
13HappyKamboj <13happykamboj@gmail.com>
2026-06-06 08:29:27 +0530

    Added Day_10 programs

---
+++ DAY_10/code40.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    //Printing character pyramid
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5 - i; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < i; j++) {
+            printf("%c", j+'A');
+        }
+        for (int j = i - 2; j >= 0; j--) {
+            printf("%c", j+'A');
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
